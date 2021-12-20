634ff57979992f3c4edd3c860ad3c271ea6f94b4
yuhan-zhao <995023060@qq.com>
2021-12-20 15:15:44 -0500

    commit test 2

--- test-dev-branch.py
+++ test-dev-branch.py
@@ -2,3 +2,5 @@ print("'git branch <branch_name>' to create a new branch")
 print("'git checkout <branch_name>' to checkout the new branch")
 print("'git checkout -b <branch_name>' is shortcut to checkout+creare new branch")
 print("This is in test-dev branch.")
+
+print("commit test.")
